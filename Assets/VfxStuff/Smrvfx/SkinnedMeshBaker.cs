b5ef6ac4855192b7748644954d79e4840921868a
Sammyd <samdresser6@gmail.com>
2020-04-04 00:12:09 -0400

    wot I got right now

--- Assets/VfxStuff/Smrvfx/SkinnedMeshBaker.cs
+++ Assets/VfxStuff/Smrvfx/SkinnedMeshBaker.cs
@@ -10,7 +10,8 @@ namespace Smrvfx
         [SerializeField] SkinnedMeshRenderer _source = null;
         [SerializeField] RenderTexture _positionMap = null;
         [SerializeField] RenderTexture _velocityMap = null;
-        [SerializeField] RenderTexture _normalMap = null;
+		[SerializeField] RenderTexture _colorMap = null;
+		[SerializeField] RenderTexture _normalMap = null;
         [SerializeField] ComputeShader _compute = null;
 
         #endregion
@@ -23,15 +24,18 @@ namespace Smrvfx
         int[] _dimensions = new int[2];
 
         List<Vector3> _positionList = new List<Vector3>();
-        List<Vector3> _normalList = new List<Vector3>();
+		List<Vector3> _normalList = new List<Vector3>();
+		List<Vector3> _colorList = new List<Vector3>();
 
-        ComputeBuffer _positionBuffer1;
+		ComputeBuffer _positionBuffer1;
         ComputeBuffer _positionBuffer2;
         ComputeBuffer _normalBuffer;
+		ComputeBuffer _colorBuffer;
 
-        RenderTexture _tempPositionMap;
+		RenderTexture _tempPositionMap;
         RenderTexture _tempVelocityMap;
         RenderTexture _tempNormalMap;
+		RenderTexture _tempColorMap;
 
         #endregion
 
@@ -50,29 +54,37 @@ namespace Smrvfx
             Utility.TryDispose(_positionBuffer1);
             Utility.TryDispose(_positionBuffer2);
             Utility.TryDispose(_normalBuffer);
+			Utility.TryDispose(_colorBuffer);
 
-            Utility.TryDestroy(_tempPositionMap);
+			Utility.TryDestroy(_tempPositionMap);
             Utility.TryDestroy(_tempVelocityMap);
             Utility.TryDestroy(_tempNormalMap);
+			Utility.TryDestroy(_tempColorMap);
 
-            _positionBuffer1 = null;
+			_positionBuffer1 = null;
             _positionBuffer2 = null;
             _normalBuffer = null;
+			_colorBuffer = null;
 
-            _tempPositionMap = null;
+			_tempPositionMap = null;
             _tempVelocityMap = null;
             _tempNormalMap = null;
-        }
+			_tempColorMap = null;
+		}
 
-        void Update()
-        {
-            if (_source == null) return;
+		void Update()
+		{
+			if (_source == null) return;
 
-            _source.BakeMesh(_mesh);
-            _mesh.GetVertices(_positionList);
-            _mesh.GetNormals(_normalList);
+			_source.BakeMesh(_mesh);
+			_mesh.GetVertices(_positionList);
+			_mesh.GetNormals(_normalList);
 
-            if (!CheckConsistency()) return;
+
+			_mesh.GetNormals(_colorList);
+			//_mesh.GetColors(_colorList);
+
+			if (!CheckConsistency()) return;
 
             TransferData();
 
@@ -101,10 +113,12 @@ namespace Smrvfx
                 _positionBuffer1.Dispose();
                 _positionBuffer2.Dispose();
                 _normalBuffer.Dispose();
+				_colorBuffer.Dispose();
 
                 _positionBuffer1 = null;
                 _positionBuffer2 = null;
                 _normalBuffer = null;
+				_colorBuffer = null;
             }
 
             if (_tempPositionMap != null &&
@@ -114,10 +128,12 @@ namespace Smrvfx
                 Destroy(_tempPositionMap);
                 Destroy(_tempVelocityMap);
                 Destroy(_tempNormalMap);
+				Destroy(_tempColorMap);
 
                 _tempPositionMap = null;
                 _tempVelocityMap = null;
                 _tempNormalMap = null;
+				_tempColorMap = null;
             }
 
             // Lazy initialization of temporary objects
@@ -127,41 +143,48 @@ namespace Smrvfx
                 _positionBuffer1 = new ComputeBuffer(vcount_x3, sizeof(float));
                 _positionBuffer2 = new ComputeBuffer(vcount_x3, sizeof(float));
                 _normalBuffer = new ComputeBuffer(vcount_x3, sizeof(float));
-            }
+				_colorBuffer = new ComputeBuffer(vcount_x3, sizeof(float));
+			}
 
-            if (_tempPositionMap == null)
+			if (_tempPositionMap == null)
             {
                 _tempPositionMap = Utility.CreateRenderTexture(_positionMap);
                 _tempVelocityMap = Utility.CreateRenderTexture(_positionMap);
                 _tempNormalMap = Utility.CreateRenderTexture(_positionMap);
-            }
+				_tempColorMap = Utility.CreateRenderTexture(_positionMap);
+			}
 
-            // Set data and execute the transfer task.
+			// Set data and execute the transfer task.
 
-            _compute.SetInt("VertexCount", vcount);
+			_compute.SetInt("VertexCount", vcount);
             _compute.SetMatrix("Transform", _source.transform.localToWorldMatrix);
             _compute.SetMatrix("OldTransform", _previousTransform);
             _compute.SetFloat("FrameRate", 1 / Time.deltaTime);
 
             _positionBuffer1.SetData(_positionList);
             _normalBuffer.SetData(_normalList);
+			_colorBuffer.SetData(_colorList);
 
-            _compute.SetBuffer(0, "PositionBuffer", _positionBuffer1);
+
+			_compute.SetBuffer(0, "PositionBuffer", _positionBuffer1);
             _compute.SetBuffer(0, "OldPositionBuffer", _positionBuffer2);
             _compute.SetBuffer(0, "NormalBuffer", _normalBuffer);
+			_compute.SetBuffer(0, "ColorBuffer", _colorBuffer);
 
-            _compute.SetTexture(0, "PositionMap", _tempPositionMap);
+			_compute.SetTexture(0, "PositionMap", _tempPositionMap);
             _compute.SetTexture(0, "VelocityMap", _tempVelocityMap);
             _compute.SetTexture(0, "NormalMap", _tempNormalMap);
+			_compute.SetTexture(0, "ColorMap", _tempColorMap);
 
-            _compute.Dispatch(0, mapWidth / 8, mapHeight / 8, 1);
+			_compute.Dispatch(0, mapWidth / 8, mapHeight / 8, 1);
 
             Graphics.CopyTexture(_tempPositionMap, _positionMap);
             Graphics.CopyTexture(_tempVelocityMap, _velocityMap);
             Graphics.CopyTexture(_tempNormalMap, _normalMap);
-        }
+			Graphics.CopyTexture(_tempColorMap, _colorMap);
+		}
 
-        bool _warned;
+		bool _warned;
 
         bool CheckConsistency()
         {
