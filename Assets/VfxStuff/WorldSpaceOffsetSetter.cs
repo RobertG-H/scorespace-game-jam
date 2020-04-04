31f3a167237738ae46c4022af0018b1a3181af06
Sammyd <samdresser6@gmail.com>
2020-04-04 17:16:31 -0400

    Passable Line  effect

---
+++ Assets/VfxStuff/WorldSpaceOffsetSetter.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.VFX;
+
+[RequireComponent(typeof(VisualEffect))]
+public class WorldSpaceOffsetSetter : MonoBehaviour
+{
+	Vector3 lastFramePos = Vector3.zero;
+	VisualEffect vfx;
+	PlayerController pcont;
+
+	private void Start()
+	{
+		lastFramePos = this.transform.position;
+		vfx = this.gameObject.GetComponent<VisualEffect>();
+		pcont = Object.FindObjectOfType<PlayerController>();
+	}
+
+	// Update is called once per frame
+	void Update()
+    {
+		Vector3 wsOffset = this.transform.position - lastFramePos;
+
+		vfx.SetVector3("WsOffset", wsOffset);
+		vfx.SetVector3("EmitterOffset", this.transform.position);
+		vfx.SetFloat("RawSpeed", pcont.velocity);
+
+		lastFramePos = this.transform.position;
+    }
+}
