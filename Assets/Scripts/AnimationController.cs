b0e8b59f70252fb1aa3fb02fe600dfa131072dc0
johnnybib <jamesgrahamhu@gmail.com>
2020-04-04 11:59:02 -0400

    Fixing world coordinate issues

--- Assets/Scripts/AnimationController.cs
+++ Assets/Scripts/AnimationController.cs
@@ -8,41 +8,60 @@ public class AnimationController : MonoBehaviour
     public Transform board;
     public Transform hips;
     public Transform head;
-    public Transform leftHandIK;
+    public Transform LHandIK;
     public Transform pizzaBoxBase;
+    public Transform LHandTargetForward;
+    public Transform LHandTargetBackward;
 
+    private Vector3 initHipsPos;
+    private Vector3 initLHandIKPos;
 
-    public Vector3 pizzaBoxOffset;
-    public float leftHandIKMaxDist;
-    public float pizzaBoxMaxDist;
-
-    private float initHipsY;
-    private float initLeftHandIKX;
+    private Vector3 initpizzaBoxBasePos;
 
     // Start is called before the first frame update
     void Start()
     {
-        initHipsY = hips.position.y;
-        initLeftHandIKX = leftHandIK.position.x;
-
+        initHipsPos = hips.localPosition;
+        initLHandIKPos = LHandIK.position;
+        initpizzaBoxBasePos = pizzaBoxBase.localPosition;
     }
 
     // Update is called once per frame
     void Update()
     {
-        hips.rotation = board.rotation;
-        float boardAngle = board.localEulerAngles.x*Mathf.Deg2Rad;
-        Vector3 newHipsPos = hips.position;
-        newHipsPos.y = initHipsY * Mathf.Cos(boardAngle);
-        hips.position = newHipsPos;
-
-        Vector3 newPizzaBoxBasePos = hips.position + pizzaBoxOffset;
-        newPizzaBoxBasePos.z = pizzaBoxMaxDist * Mathf.Sin(boardAngle);
-        pizzaBoxBase.position = newPizzaBoxBasePos;
         
-        Vector3 newleftHandIKPos = leftHandIK.position;
-        newleftHandIKPos.z = leftHandIKMaxDist * Mathf.Sin(boardAngle);
-        newleftHandIKPos.x = initLeftHandIKX * Mathf.Abs(Mathf.Cos(boardAngle));
-        leftHandIK.position = newleftHandIKPos;
+        float boardAngle = board.localEulerAngles.x*Mathf.Deg2Rad;
+
+        hips.localRotation = board.localRotation;
+        Vector3 newHipsPos = hips.localPosition;
+        newHipsPos.y = initHipsPos.y * Mathf.Cos(boardAngle );
+        hips.localPosition = newHipsPos;
+
+        float boardAngleDeg = board.localEulerAngles.x;
+        if(boardAngleDeg > 180)
+        {
+            boardAngleDeg -= 360;
+        }
+        Vector3 newPizzaBoxBasePos = pizzaBoxBase.localPosition;
+        newPizzaBoxBasePos.y = initpizzaBoxBasePos.y *(-0.2f/30f*Mathf.Abs(boardAngleDeg) + 1);
+
+        if(boardAngleDeg > 0)
+        {
+            newPizzaBoxBasePos.z = initpizzaBoxBasePos.y - newPizzaBoxBasePos.y;
+        }
+        else
+        {
+            newPizzaBoxBasePos.z = -(initpizzaBoxBasePos.y - newPizzaBoxBasePos.y);
+        }
+        pizzaBoxBase.localPosition = newPizzaBoxBasePos;
+        
+        if(boardAngleDeg > 0)
+        {
+            LHandIK.position = Vector3.Lerp(initLHandIKPos, LHandTargetForward.position, Mathf.Abs(boardAngleDeg/30f));
+        }
+        else
+        {
+            LHandIK.position = Vector3.Lerp(initLHandIKPos, LHandTargetBackward.position, Mathf.Abs(boardAngleDeg/30f));
+        }
     }
 }
